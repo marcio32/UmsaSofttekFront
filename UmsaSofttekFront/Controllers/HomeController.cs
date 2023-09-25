2e43afe3587dfff7639d38aa3bc5279642809904
Marcio Abriola <marcioabriola@gmail.com>
2023-09-25 10:00:47 -0300

    Subo Clase

--- UmsaSofttekFront/Controllers/HomeController.cs
+++ UmsaSofttekFront/Controllers/HomeController.cs
@@ -17,12 +17,8 @@ namespace UmsaSofttekFront.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+    
+    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ UmsaSofttekFront/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UmsaSofttekFront.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+        public IActionResult Ingresar(LoginDto login)
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
